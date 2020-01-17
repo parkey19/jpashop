9814facb64bf5a875988693a9828b8ffa0b5d89b
SungJinPark <kkttqq@gmail.com>
2020-01-18 01:53:53 +0900

    add team

---
+++ jpashop/domain/Team.java
@@ -0,0 +1,46 @@
+package jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by parkey19 on 2020/01/17.
+ */
+@Entity
+public class Team {
+    @Id
+    @Column(name = "TEAM_ID")
+    private long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+}
